484300370ce177bac9037acfeb532976ed224dce
bjornvangompel <bjorn.vangompel@gmail.com>
2017-04-20 23:43:53 +0200

    json simple jar

--- src/app/TeDaGenFunctionConfig.java
+++ src/app/TeDaGenFunctionConfig.java
@@ -24,7 +24,7 @@ public class TeDaGenFunctionConfig {
     }
 
     public boolean readJsonConfig ()
-            throws FileNotFoundException,
+            throws
             IOException, ParseException {
 
         JSONParser parser = new JSONParser();
